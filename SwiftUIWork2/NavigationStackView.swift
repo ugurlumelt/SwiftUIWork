28a44911cf3575bb3984a614125e5f140ddfcf50
Meltem <meltemak8@gmail.com>
2024-07-19 15:53:15 +0200

    NavigationStack features are added

---
+++ SwiftUIWork2/NavigationStackView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackView.swift
+//  SwiftUIWork2
+//
+//  Created by Meltem Uğurlu on 19.07.2024.
+//
+
+import SwiftUI
+
+struct NavigationStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackView()
+}
