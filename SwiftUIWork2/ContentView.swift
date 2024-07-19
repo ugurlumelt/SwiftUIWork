f666eb685468bce62eaa981920a36ab3cfaf19b8
Meltem <meltemak8@gmail.com>
2024-07-19 13:59:39 +0200

    Initial Commit

---
+++ SwiftUIWork2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIWork2
+//
+//  Created by Meltem Uğurlu on 19.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIWork2/SwiftUIWork2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWork2App.swift
+//  SwiftUIWork2
+//
+//  Created by Meltem Uğurlu on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWork2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
