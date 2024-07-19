a74d2eb61693430ce28cd9975d20a3ea3e3def1b
Meltem <meltemak8@gmail.com>
2024-07-19 15:53:57 +0200

    NavigationStack features are added

--- SwiftUIWork2/NavigationStackView.swift
+++ SwiftUIWork2/NavigationStackView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct NavigationStackView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    
+    init(){
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .blue
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+         
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
     }
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }.navigationTitle("Page1")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Button("back"){
+                            
+                        }.foregroundColor(.orange)
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button("settings"){
+                            
+                        }.foregroundColor(.orange)
+                    }
+                }
+        }
+    }
+  
 }
 
 #Preview {
