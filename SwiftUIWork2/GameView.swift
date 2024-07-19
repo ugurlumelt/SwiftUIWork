8c943d09665d4704fa1cbef3de8639494feab56e
Meltem <meltemak8@gmail.com>
2024-07-19 14:10:54 +0200

    Views are added in the app

---
+++ SwiftUIWork2/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  SwiftUIWork2
+//
+//  Created by Meltem Uğurlu on 19.07.2024.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
---
+++ SwiftUIWork2/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUIWork2
+//
+//  Created by Meltem Uğurlu on 19.07.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
